a2aab0212b7df7675dbb258b3ed906e11a1e4337
Freewind <nowindlee@gmail.com>
2023-09-25 18:03:33 +0800

    upgrade to latest version

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,100 +1,86 @@
-import React, {FC} from 'react'
+import React, {FC, useReducer} from 'react'
 
-import {
-  createTable,
-  getCoreRowModel,
-  useTableInstance,
-  CoreColumnDef,
-  Render,
-  VisibilityColumnDef,
-  ColumnSizingColumnDef,
-  ColumnPinningColumnDef,
-  FiltersColumnDef,
-  SortingColumnDef,
-  GroupingColumnDef,
+import {createColumnHelper, flexRender, getCoreRowModel, useReactTable} from '@tanstack/react-table'
+import {defaultData, Person} from './data';
 
-} from '@tanstack/react-table'
-import {Person, defaultData} from './data';
-import {ColumnDef} from '@tanstack/table-core/build/types/types';
-import {TableInstance} from '@tanstack/table-core';
-import '@tanstack/table-core/build/types/createTable';
-
-const table = createTable().setRowType<Person>()
-
+const columnHelper = createColumnHelper<Person>();
 
 export const defaultColumns = [
-  table.createDataColumn('firstName', {
-    cell: info => info.getValue(),
-    footer: props => props.column.id,
-  }),
-  table.createDataColumn(row => row.lastName, {
-    id: 'lastName',
-    cell: info => info.getValue(),
-    header: () => <span>Last Name</span>,
-    footer: props => props.column.id,
-  }),
-  table.createDataColumn('age', {
-    header: () => 'Age',
-    footer: props => props.column.id,
-  }),
-  table.createDataColumn('visits', {
-    header: () => <span>Visits</span>,
-    footer: props => props.column.id,
-  }),
-  table.createDataColumn('status', {
-    header: 'Status',
-    footer: props => props.column.id,
-  }),
-  table.createDataColumn('progress', {
-    header: 'Profile Progress',
-    footer: props => props.column.id,
-  }),
+    columnHelper.accessor('firstName', {
+        cell: info => info.getValue(),
+        footer: props => props.column.id,
+    }),
+    columnHelper.accessor(row => row.lastName, {
+        id: 'lastName',
+        cell: info => info.getValue(),
+        header: () => <span>Last Name</span>,
+        footer: props => props.column.id,
+    }),
+    columnHelper.accessor('age', {
+        header: () => 'Age',
+        footer: props => props.column.id,
+    }),
+    columnHelper.accessor('visits', {
+        header: () => <span>Visits</span>,
+        footer: props => props.column.id,
+    }),
+    columnHelper.accessor('status', {
+        header: 'Status',
+        footer: props => props.column.id,
+    }),
+    columnHelper.accessor('progress', {
+        header: 'Profile Progress',
+        footer: props => props.column.id,
+    }),
 ]
 
 export const Table: FC = () => {
 
-  const instance = useTableInstance(table, {
-    data: defaultData,
-    columns: defaultColumns,
-    getCoreRowModel: getCoreRowModel(),
-  })
+    const forceRerender = useReducer(() => [], [])[1]
 
-  return (
-    <div>
-      <table border={1}>
-        <thead>
-        {instance.getHeaderGroups().map(headerGroup => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map(header => (
-              <th key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : header.renderHeader()}
-              </th>
-            ))}
-          </tr>
-        ))}
-        </thead>
-        <tbody>
-        {instance.getRowModel().rows.map(row => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map(cell => (
-              <td key={cell.id}>{cell.renderCell()}</td>
-            ))}
-          </tr>
-        ))}
-        </tbody>
-        <tfoot>
-        {instance.getFooterGroups().map(footerGroup => (
-          <tr key={footerGroup.id}>
-            {footerGroup.headers.map(header => (
-              <th key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : header.renderFooter()}
-              </th>
-            ))}
-          </tr>
-        ))}
-        </tfoot>
-      </table>
-      <div/>
-    </div>
-  )
+    const table = useReactTable({
+        data: defaultData,
+        columns: defaultColumns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+        <div>
+            <table border={1}>
+                <thead>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <tr key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <th key={header.id} colSpan={header.colSpan}>
+                                {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody>
+                {table.getRowModel().rows.map(row => (
+                    <tr key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+                <tfoot>
+                {table.getFooterGroups().map(footerGroup => (
+                    <tr key={footerGroup.id}>
+                        {footerGroup.headers.map(header => (
+                            <th key={header.id} colSpan={header.colSpan}>
+                                {header.isPlaceholder ? null : flexRender(header.column.columnDef.footer, header.getContext())}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </tfoot>
+            </table>
+            <div/>
+            <button onClick={() => forceRerender()}>Force rerender</button>
+        </div>
+    )
 };
